d35d778c2b655f4471c4e1a58bf616932428b837
Diego_Montoya <0258630@up.edu.mx>
2023-12-05 11:36:47 -0600

    Se añade css

--- client/src/components/More_content.jsx
+++ client/src/components/More_content.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from './Navbar';
 import Footer from "./Footer";
 import Videos from "./Videos";
+import '../More_content.css';
 
 function More_content(){
     return (
